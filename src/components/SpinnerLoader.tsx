2e3b8d363a694afcee6ec8be34b14c9e1d33c57a
mmajudith <mmajudith2000@gmail.com>
2023-03-09 16:08:40 -0800

    refactor
    Refactored some code

--- src/components/SpinnerLoader.tsx
+++ src/components/SpinnerLoader.tsx
@@ -1,8 +1,7 @@
 const SpinnerLoader = () => {
     return(
-        <div className="mt-16 flex justify-center items-center">
+        <div className="w-full h-auto m-auto mt-16 flex flex-col justify-center items-center">
             <div className="w-[100px] sm:w-[200px] h-[100px] sm:h-[200px] rounded-full sm:border-t-8 border-t-4 sm:border-r-8 border-r-4 border-very-dark-blue-text dark:border-White border-solid animate-spin">
-
             </div>
         </div>
     )
